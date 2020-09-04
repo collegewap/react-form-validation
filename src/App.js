89285a9998b98164496dd11645ebca9547d1fdb4
Abhishek EH <collegewap@gmail.com>
2020-09-04 20:36:23 +0530

    All validations and bundle analyzer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import "./App.css";
-import { UPDATE_FORM, onInputChange, onFocusOut } from "./lib/formUtils";
+import {
+  UPDATE_FORM,
+  onInputChange,
+  onFocusOut,
+  validateInput,
+} from "./lib/formUtils";
 
 /**
  * The initial state of the form
@@ -49,12 +54,53 @@ const formsReducer = (state, action) => {
 function App() {
   const [formState, dispatch] = useReducer(formsReducer, initialState);
 
-  console.table({ "name state": formState.name });
+  const [showError, setShowError] = useState(false);
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); //prevents the form from submitting
+
+    let isFormValid = true;
+
+    for (const name in formState) {
+      const item = formState[name];
+      const { value } = item;
+      const { hasError, error } = validateInput(name, value);
+      if (hasError) {
+        isFormValid = false;
+      }
+      if (name) {
+        dispatch({
+          type: UPDATE_FORM,
+          data: {
+            name,
+            value,
+            hasError,
+            error,
+            touched: true,
+            isFormValid,
+          },
+        });
+      }
+    }
+    if (!isFormValid) {
+      setShowError(true);
+    } else {
+      //Logic to submit the form to backend
+    }
+
+    // Hide the error message after 5 seconds
+    setTimeout(() => {
+      setShowError(false);
+    }, 5000);
+  };
 
   return (
     <div className="App">
       <h1 className="title">Sign Up</h1>
-      <form>
+      {showError && !formState.isFormValid && (
+        <div className="form_error">Please fill all the fields correctly</div>
+      )}
+      <form onSubmit={(e) => formSubmitHandler(e)}>
         <div className="input_wrapper">
           <label htmlFor="name">Name:</label>
           <input
@@ -80,7 +126,16 @@ function App() {
             name="email"
             id="email"
             value={formState.email.value}
+            onChange={(e) => {
+              onInputChange("email", e.target.value, dispatch, formState);
+            }}
+            onBlur={(e) => {
+              onFocusOut("email", e.target.value, dispatch, formState);
+            }}
           />
+          {formState.email.touched && formState.email.hasError && (
+            <div className="error">{formState.email.error}</div>
+          )}
         </div>
         <div className="input_wrapper">
           <label htmlFor="password">Password:</label>
@@ -89,7 +144,16 @@ function App() {
             name="password"
             id="password"
             value={formState.password.value}
+            onChange={(e) => {
+              onInputChange("password", e.target.value, dispatch, formState);
+            }}
+            onBlur={(e) => {
+              onFocusOut("password", e.target.value, dispatch, formState);
+            }}
           />
+          {formState.password.touched && formState.password.hasError && (
+            <div className="error">{formState.password.error}</div>
+          )}
         </div>
         <div className="input_wrapper">
           <label htmlFor="mobile">Mobile:</label>
@@ -98,7 +162,16 @@ function App() {
             name="mobile"
             id="mobile"
             value={formState.mobile.value}
+            onChange={(e) => {
+              onInputChange("mobile", e.target.value, dispatch, formState);
+            }}
+            onBlur={(e) => {
+              onFocusOut("mobile", e.target.value, dispatch, formState);
+            }}
           />
+          {formState.mobile.touched && formState.mobile.hasError && (
+            <div className="error">{formState.mobile.error}</div>
+          )}
         </div>
         <div className="input_wrapper">
           <label className="toc">
@@ -106,9 +179,15 @@ function App() {
               type="checkbox"
               name="terms"
               checked={formState.terms.value}
+              onChange={(e) => {
+                onFocusOut("terms", e.target.checked, dispatch, formState);
+              }}
             />
             Accept terms and conditions
           </label>
+          {formState.terms.touched && formState.terms.hasError && (
+            <div className="error">{formState.terms.error}</div>
+          )}
         </div>
         <div className="input_wrapper">
           <input className="submit_btn" type="submit" value="Sign Up" />
--- src/lib/formUtils.js
+++ src/lib/formUtils.js
@@ -26,7 +26,7 @@ export const onInputChange = (name, value, dispatch, formState) => {
   });
 };
 
-const validateInput = (name, value) => {
+export const validateInput = (name, value) => {
   let hasError = false,
     error = "";
   switch (name) {
@@ -34,9 +34,58 @@ const validateInput = (name, value) => {
       if (value.trim() === "") {
         hasError = true;
         error = "Name cannot be empty";
-      } else if (!/[a-zA-Z ]/.test(value)) {
+      } else if (!/^[a-zA-Z ]+$/.test(value)) {
         hasError = true;
         error = "Invalid Name. Avoid Special characters";
+      } else {
+        hasError = false;
+        error = "";
+      }
+      break;
+    case "email":
+      if (value.trim() === "") {
+        hasError = true;
+        error = "Email cannot be empty";
+      } else if (
+        !/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(
+          value
+        )
+      ) {
+        hasError = true;
+        error = "Invalid Email";
+      } else {
+        hasError = false;
+        error = "";
+      }
+      break;
+    case "password":
+      if (value.trim() === "") {
+        hasError = true;
+        error = "Password cannot be empty";
+      } else if (value.trim().length < 8) {
+        hasError = true;
+        error = "Password must have at least 8 characters";
+      } else {
+        hasError = false;
+        error = "";
+      }
+      break;
+    case "mobile":
+      if (value.trim() === "") {
+        hasError = true;
+        error = "Mobile cannot be empty";
+      } else if (!/^[0-9]{10}$/.test(value)) {
+        hasError = true;
+        error = "Invalid Mobile Number. Use 10 digits only";
+      } else {
+        hasError = false;
+        error = "";
+      }
+      break;
+    case "terms":
+      if (!value) {
+        hasError = true;
+        error = "You must accept terms and conditions";
       } else {
         hasError = false;
         error = "";
