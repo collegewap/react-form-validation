969e18dc03f67a3c3604eaad48065cf692bc0d7c
Abhishek EH <collegewap@gmail.com>
2020-09-04 14:47:07 +0530

    Form binding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,82 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
+/**
+ * The initial state of the form
+ * value: stores the value of the input field
+ * touched: indicates whether the user has tried to input anything in the field
+ * hasError: determines whether the field has error.
+ *           Defaulted to true since all fields are mandatory and are empty on page load.
+ * error: stores the error message
+ * isFormValid: Stores the validity of the form at any given time.
+ */
+const initialState = {
+  name: { value: "", touched: false, hasError: true, error: "" },
+  email: { value: "", touched: false, hasError: true, error: "" },
+  password: { value: "", touched: false, hasError: true, error: "" },
+  mobile: { value: "", touched: false, hasError: true, error: "" },
+  terms: { value: false, touched: false, hasError: true, error: "" },
+  isFormValid: false,
+};
+
+/**
+ * Reducer which will perform form state update
+ */
+const formsReducer = (state, action) => {
+  return state;
+};
+
 function App() {
+  const [formState, dispatch] = useReducer(formsReducer, initialState);
+
   return (
     <div className="App">
       <h1 className="title">Sign Up</h1>
       <form>
         <div className="input_wrapper">
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formState.name.value}
+          />
         </div>
         <div className="input_wrapper">
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formState.email.value}
+          />
         </div>
         <div className="input_wrapper">
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formState.password.value}
+          />
         </div>
         <div className="input_wrapper">
           <label htmlFor="mobile">Mobile:</label>
-          <input type="text" name="mobile" id="mobile" />
+          <input
+            type="text"
+            name="mobile"
+            id="mobile"
+            value={formState.mobile.value}
+          />
         </div>
         <div className="input_wrapper">
           <label className="toc">
-            <input type="checkbox" name="terms" /> Accept terms and conditions
+            <input
+              type="checkbox"
+              name="terms"
+              checked={formState.terms.value}
+            />
+            Accept terms and conditions
           </label>
         </div>
         <div className="input_wrapper">
